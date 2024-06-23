e0b810adc1d546e2cef87930c3ba43b0e5242e07
shirkeaaarya25 <shirkeaarya12@gmail.com>
2024-06-23 23:25:12 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1 id="main_title">CRUD Application</h1>
+    <div class="container">
